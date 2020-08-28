42bd47aae3ebadc1427b66217be7f5f4e7a0ba78
Sebastian Grana <sebastiangrana1@gmail.com>
2020-08-28 09:17:26 -0400

    remove unnecessary console.log

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 axios
 .get(`https://api.kanye.rest`)
 .then((res)=> {
-    console.log(res.data.quote)
     document.querySelector('#quote').innerHTML = `"${res.data.quote}" -Random Quote from Kanye West`
 })
